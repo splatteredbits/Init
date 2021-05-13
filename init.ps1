57091bf36c11ba72fe28ca7f8d3ed61f203a9e5a
Aaron Jensen <ajensen@webmd.net>
2021-05-12 19:00:11 -0700

    New Bespoke hotness.

--- init.ps1
+++
@@ -1,6 +0,0 @@
-
-$expandAppAcrossMonitorsScriptPath = Join-Path -Path $PSScriptRoot -ChildPath 'ExpandWindowAcrossMonitors.Win+Shift+Up.ahk'
-
-Install-BespokeFileLink -Path $expandAppAcrossMonitorsScriptPath `
-                        -DestinationDirectory ([Environment]::GetFolderPath('Startup')) `
-                        -Invoke
